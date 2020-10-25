7efe043066fd8dc726e2c9d76eef4cad220b53a0
MARCOS MACHADO MARTINS <marcosm.caixa@gmail.com>
2020-10-25 16:49:49 -0300

    NASA PICTURE OF THE DAY

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:dioexp/http.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-import 'routes/request.dart';
+import 'package:intl/intl.dart';
 
 _parseAndDecode(String response) {
   return jsonDecode(response);
@@ -26,8 +25,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dio demo',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: MyHomePage(title: 'Dio demo home page'),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: MyHomePage(title: 'NASA PICTURE OF THE DAY'),
     );
   }
 }
@@ -46,53 +45,103 @@ class _MyHomePageState extends State<MyHomePage> {
   String _explanation = "";
   String _title = "";
   String _url = "";
-  DateTime _date = DateTime.now();
+  String _date;
+
+  final DateFormat formatter = DateFormat('yyyy-MM-dd');
+
+  _formatDate(DateTime date) {
+    _date = formatter.format(date);
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    showDatePicker(
+        context: context,
+        initialDate: _setMidweekDate(DateTime.now()),
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime.now(),
+        cancelText: "Cancelar",
+        confirmText: "Confirmar",
+        selectableDayPredicate: (DateTime val) {
+          if (val.weekday == DateTime.saturday ||
+              val.weekday == DateTime.sunday) {
+            return false;
+          }
+          return true;
+        }).then((value) {
+      _formatDate(value);
+      _getData();
+    });
+  }
+
+  _getData() {
+    dio
+        .get<Map<String, dynamic>>(
+            "https://api.nasa.gov/planetary/apod?api_key=w42dJezDhZsu7eEBdueADBUSPZqH7fetdplKfGcy&date=$_date")
+        //"https://api.nasa.gov/planetary/apod?api_key=w42dJezDhZsu7eEBdueADBUSPZqH7fetdplKfGcy")
+        .then(
+      (response) {
+        setState(
+          () {
+            _title = response.data["title"];
+            _copyright = response.data["copyright"];
+            _explanation = response.data["explanation"];
+            _url = response.data["url"];
+          },
+        );
+      },
+    ).catchError(print);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _formatDate(_setMidweekDate(DateTime.now()));
+  }
+
+  DateTime _setMidweekDate(DateTime date){
+    DateTime d = date.subtract(
+      Duration(days: _checkWhetherSaturdaySundayOrWeekday(date)));
+    return d;
+  }
+
+  int _checkWhetherSaturdaySundayOrWeekday(DateTime data){
+    if (data.weekday == DateTime.saturday){
+      return 1;
+    }
+    if (data.weekday == DateTime.sunday){
+      return 2;
+    }
+    return 0;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              //selecionar a data primeiro
-              RaisedButton(
-                child: Text('Mostar imagem do dia'),
-                onPressed: () {
-                  dio
-                      .get<Map<String, dynamic>>(
-                          "https://api.nasa.gov/planetary/apod?api_key=w42dJezDhZsu7eEBdueADBUSPZqH7fetdplKfGcy&date=$_date")
-                      //"https://api.nasa.gov/planetary/apod?api_key=w42dJezDhZsu7eEBdueADBUSPZqH7fetdplKfGcy")
-                      .then(
-                    (response) {
-                      setState(
-                        () {
-                          print('data:');
-                          print(response);
-                          _title = response.data["title"];
-                          _copyright = response.data["copyright"];
-                          _explanation = response.data["explanation"];
-                          _url = response.data["url"];
-                        },
-                      );
-                    },
-                  ).catchError(print);
-                },
+      appBar: AppBar(title: Text(widget.title)),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            RaisedButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Selecionar a data'),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.all(10),
+                children: [
+                  Text(_title ?? 'Título: $_title'),
+                  Container(
+                    child: _url != "" ? Image.network(_url) : null,
+                  ),
+                  Text(_copyright ?? 'Autor: $_copyright'),
+                  Text(_explanation ?? 'Informação: $_explanation'),
+                ],
               ),
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.all(10),
-                  children: [
-                    Text('Título: $_title'),
-                    Image.network(_url),
-                    Text('Autor: $_copyright'),
-                    Text('Informação: $_explanation'),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
